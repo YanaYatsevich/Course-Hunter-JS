d3689460cbfc147bd28e73c65c7072f944fe34ce
Yana <jana.jacewicz@gmail.com>
2021-01-04 12:14:58 +0300

    CH JS Lesson 12

---
+++ Lesson 11/script11.js
@@ -0,0 +1,33 @@
+"use strict";
+
+let num = 50;
+
+// while (num < 55) {
+   //  console.log(num);
+    // num++;
+// }
+
+/* do {
+    console.log(num);
+    num++;
+}
+while (num < 55);
+
+for(let i=1; i<8; i++) {
+    console.log(i);
+}  */
+
+/* for(let i=1; i<8; i++) {
+    console.log(num);
+    num++;
+}  */
+
+for(let i=1; i<10; i++) {
+    if (i === 6){
+        //break;
+        continue; //позволяет пропустить шаг, который нам не нужен, но код продолжит работать до конца
+    }
+
+    console.log(i);
+    
+} 
